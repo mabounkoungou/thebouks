5219daa8b856126f030ed39796453f268f9726d9
Raadbouks <raadbouks@gmail.com>
2025-10-19 16:48:26 +0000

    UPdate CV

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -55,13 +55,14 @@ const Profile = () => {
             >
               My Projects
             </a>
-         <a
+     <a
   className={`btn xxs:btn-lg px-6 max-xs:px-2 xxs:py-3 hover:border-picto-primary bg-white duration-300 transition-all hover:text-picto-primary ms-4 text-xs xxs:text-[14px] sm:text-[16px]`}
-  download="JABBARCV.pdf"
-  href="/JABBARCV.pdf"
+  download="MuhammedJabbarcv.pdf"
+  href="https://mabounkoungou.github.io/thebouks/MuhammedJabbarcv.pdf"
 >
   <FontAwesomeIcon icon={faDownload} /> Download CV
 </a>
+
           </div>
         </div>
       </div>
