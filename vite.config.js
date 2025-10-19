b5ef794d59ff901b370ef23dc36ad7fe7dc453ac
Raadbouks <raadbouks@gmail.com>
2025-10-19 11:24:59 +0000

    base

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    // 👇 Set base to "/" for Vercel
-    base: "/",
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: true,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  base: "/", // ✅ required for Vercel
+  plugins: [react(), tailwindcss()],
 });
