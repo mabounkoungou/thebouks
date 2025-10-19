aad89d203cdabb50fb82430e5eb354d41f036979
Raadbouks <raadbouks@gmail.com>
2025-10-19 11:31:48 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: "/", // ✅ required for Vercel
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    // 👇 Set base to "/" for Vercel
+    base: "/bouks",
+    plugins: [react(), tailwindcss()],
+    server: {
+      open: true,
+      host: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
